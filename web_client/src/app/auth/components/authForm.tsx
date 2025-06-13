30fbb0755f43c99dcb5a1b5e4730d3ef20b31ef2
MyAngelShibuyaKanon <99974252+MyAngelShibuyaKanon@users.noreply.github.com>
2025-06-13 12:00:32 +0800

    finally got db working

--- web_client/src/app/auth/components/authForm.tsx
+++ web_client/src/app/auth/components/authForm.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { JSX } from "react/jsx-runtime";
 import Form from "next/form";
+import Link from 'next/link'
 type authFormType = "SignIn" | "SignUp";
 
 export function AuthForm({ formType }: { formType: authFormType, }): JSX.Element {
@@ -12,14 +13,17 @@ export function AuthForm({ formType }: { formType: authFormType, }): JSX.Element
   }
 
   return (
-    <Form action={onSubmit} className="flex flex-col text-center gap-y-4 text-black" >
+    <Form action={onSubmit} className="flex flex-col text-center gap-y-4 text-black w-64" >
       <h1 className="text-white">
         {isSignIn ? "Sign In" : "Register"}
       </h1>
       <input className="bg-white" name="username" />
       <input className="bg-white" name="password" />
       <button className="bg-white text-black">{isSignIn ? "Sign In" : "Register"} </button>
-
+      <p className="flex flex-row text-white text-sm justify-center gap-2">
+        <p>{isSignIn ? "Don't have an account?" : "Already have an account?"}</p>
+        {isSignIn ? <Link href={"/auth/register"}>Sign Up!</Link> : <Link href={"/auth/login"}>Sign In!</Link>}
+      </p>
     </Form>
   )
 }
